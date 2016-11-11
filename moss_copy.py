595fe67e8591eb5d3bc5879ab67befdce5d15aab
mmj9@duke.edu <mmj9@duke.edu>
2016-11-10 21:55:31 -0500

    copies of ultrasound for us to make changes to without causing conflict

---
+++ moss_copy.py
@@ -0,0 +1,39 @@
+# File for all function definitions used in ultrasound_main.py
+
+def read_jsonfile(infile):
+    """ Read data parameters from json file
+
+    :param infile: input json filename (str)
+    :returns: c, fs, axial_samples, beam_spacing, num_beams
+    """
+
+    import json
+
+    with open(infile) as file:
+        params = json.load(file)
+
+    c = params['c']
+    fs = params['fs']
+    axial_samples = params['axial_samples']
+    beam_spacing = params['beam_spacing']
+    num_beams = params['num_beams']
+
+    return c, fs, axial_samples, beam_spacing, num_beams
+
+def read_rffile(rffile):
+    """ Read RF data from binary file
+
+    :param rffile: input rf binary filename (str)
+    :returns: rfdata
+    """
+
+    rfdata = []
+
+    with open(rffile,'rb') as file:
+        data = file.read(2)
+        while data:
+            rf = int.from_bytes(data, byteorder = 'little', signed=True)
+            rfdata.append(rf)
+            data = file.read(2)
+
+    return rfdata
