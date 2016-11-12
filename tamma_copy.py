bca8108de93adb7f93784d3bee4f5aeb20ce7e8b
tk114@duke.edu <tk114@duke.edu>
2016-11-12 14:41:16 -0500

    update tamma_copy

--- tamma_copy.py
+++ tamma_copy.py
@@ -37,3 +37,53 @@ def read_rffile(rffile):
             data = file.read(2)
 
     return rfdata
+
+def calc_lat_position(beam_spacing, num_beams):
+    """
+    Calculate lateral position and length
+
+    :param beam_spacing: spacing between lateral beams (m)
+    :param num_beams: number of lateral beams
+    :returns: lateral (list), total_lateral (float)
+    """
+
+    import numpy as np
+
+    start_position = -((num_beams-1)/2)*beam_spacing
+    end_position = ((num_beams)/2)*beam_spacing
+    lat = np.arange(start_position, end_position, beam_spacing)
+    lateral = list(lat)
+
+    total_lateral = float(beam_spacing*num_beams)
+
+    return lateral, total_lateral
+
+def calc_axial_position(c, fs, axial_samples):
+    """
+    Calculate axial position and length
+
+    :param c: souns speed (m/s)
+    :param fs: sampling frequency (Hz)
+    :param axial_samples: number of samples in depth
+    :returns: axial (list), total_depth (float)
+    """
+
+    import numpy as np
+
+    try:
+        delta_t = 1/fs
+    except ZeroDivisionError:
+        print('Input Parameter Error! Improbable sampling frequency')
+        return 0.0, 0.0
+
+    distance_btw_sample = c*delta_t
+
+    start_position = 0
+    end_position = distance_btw_sample*(axial_samples)
+    ax = np.arange(start_position, end_position, distance_btw_sample)
+    axial = ax.tolist()
+
+    total_depth = float((axial_samples-1)*distance_btw_sample)
+
+    return axial, total_depth
+
