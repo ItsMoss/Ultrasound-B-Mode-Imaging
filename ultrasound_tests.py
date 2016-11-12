6caec065922bbe4322ff4945ef79bf3782630506
tk114@duke.edu <tk114@duke.edu>
2016-11-12 10:48:27 -0500

    move unit test to ultrasound_tests.py

--- ultrasound_tests.py
+++ ultrasound_tests.py
@@ -77,3 +77,20 @@ def test_calc_lat_position():
     [lat3, total_lat3] = us.calc_lat_position(0.5, 5)
     assert lat3 == [-1.0, -0.5, 0.0, 0.5, 1.0]
     assert total_lat3 == 2.5
+
+def test_calc_axial():
+    """
+    Tests cal_axial_position functionality from ultrasound.py
+    """
+
+    [ax1, total_depth1] = us.calc_axial_position(1, 100, 10)
+    assert ax1 == [0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+    assert total_depth1 == 0.09
+
+    [ax2, total_depth2] = us.calc_axial_position(0.5, 50, 10)
+    assert ax2 == [0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+    assert total_depth2 == 0.09
+
+    [ax3, total_depth3] = us.calc_axial_position(1, 0, 10)
+    assert ax3 == 0.0
+    assert total_depth3 == 0.0
