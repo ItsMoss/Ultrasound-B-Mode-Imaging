bf53689dd8a430c9dd590d4c84abd0edb13c2e4e
tk114@duke.edu <tk114@duke.edu>
2016-11-12 08:42:53 -0500

    move parse_main from ultrasound_main.py to ultrasound.py

--- ultrasound.py
+++ ultrasound.py
@@ -60,3 +60,31 @@ def init_matrix(x_len, y_len):
     :return list: 2-D matrix with all values initialized to 0
     """
     return [[0 for x in range(x_len)] for y in range(y_len)]
+
+
+def parse_main():
+    """This function sets default values or accepts
+    user inputs for the variables in main function
+
+    :returns: args
+    """
+    import argparse as ap
+
+    par = ap.ArgumentParser(description="Accept user input argument",
+                            formatter_class=ap.ArgumentDefaultsHelpFormatter)
+
+    par.add_argument("--json_filename",
+                     dest="json_filename",
+                     help="Data acquisition metadata in json file",
+                     type=str,
+                     default="bmode.json")
+
+    par.add_argument("--rf_filename",
+                     dest="rf_filename",
+                     help="RF binary filename",
+                     type=str,
+                     default="rfdat.bin")
+
+    args = par.parse_args()
+
+    return args
--- ultrasound_main.py
+++ ultrasound_main.py
@@ -1,34 +1,8 @@
 # This file is for main program
 
+
 def main():
     pass
-
-def parse_main():
-    """This function sets default values or accepts
-    user inputs for the variables in main function
-
-    :returns: args
-    """
-    import argparse as ap
-
-    par = ap.ArgumentParser(description="Accept user input argument",
-                            formatter_class=ap.ArgumentDefaultsHelpFormatter)
-
-    par.add_argument("--json_filename",
-                     dest="json_filename",
-                     help="Data acquisition metadata in json file",
-                     type=str,
-                     default="bmode.json")
-
-    par.add_argument("--rf_filename",
-                     dest="rf_filename",
-                     help="RF binary filename",
-                     type=str,
-                     default="rfdat.bin")
-
-    args = par.parse_args()
-
-    return args
 
 
 if __name__ == "__main__":
